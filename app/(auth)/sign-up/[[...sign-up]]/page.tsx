5b64968f6ebfccee21df06ffdcb7d3f08fb54e44
oThinas <thiagomartinsprado@gmail.com>
2024-05-30 02:53:17 -0300

    feat: add sign-in, sign-up and sign-out

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,36 @@
+/** Core */
+import { ClerkLoaded, ClerkLoading, SignUp } from '@clerk/nextjs';
+import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
+
+export default function SignUpPage() {
+  return (
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
+        <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold text-[#2E2A27]">
+            Welcome Back!
+          </h1>
+        </div>
+
+        <p className="text-base text-[#7E8CA0]">
+          Log in or Create account to get back to your dashboard!
+        </p>
+
+        <div className="mt-8 flex items-center justify-center">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+
+      <div className="hidden h-full items-center justify-center bg-blue-600 lg:flex">
+        <Image src="/logo.svg" width={100} height={100} alt="Logo" />
+      </div>
+    </div>
+  );
+}
